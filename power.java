606eaad549375fb7ee63d1e94e5fe86e8271d231
Asha <43291441+Asha2805@users.noreply.github.com>
2018-10-11 09:09:50 -0400

    Update and rename README.md to power.java

---
+++ power.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Power
+{
+public static void main(String[] args)
+{
+int a,b,ans;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+b=s.nextInt();
+ans=pow(a,b);
+System.out.println(ans);
+}
+}
